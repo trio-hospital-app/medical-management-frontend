24c2d3e22441516bd46ba2011bf2982965dfa125
chisom chima <chimachisom360@gmail.com>
2023-11-17 14:31:31 +0100

    patient

--- src/components/ui/modals/basicModal.tsx
+++ src/components/ui/modals/basicModal.tsx
@@ -3,28 +3,35 @@ import { Button, Modal } from 'flowbite-react';
 
 interface BasicModalProps {
   children: ReactNode;
+  title: string,
   setOpenModal: React.Dispatch<React.SetStateAction<boolean>>;
   openModal: boolean;
   submitTitle: string;
   showCancelButton?:boolean;
   showSubmitButton?:boolean;
+  cancelTitle: string;
 }
 
-function BasicModal({ children, setOpenModal, openModal, submitTitle, showCancelButton, showSubmitButton }: BasicModalProps) {
+function BasicModal({ children, title, setOpenModal, openModal, cancelTitle, submitTitle, showCancelButton, showSubmitButton }: BasicModalProps) {
   return (
     <>
-      <Button onClick={() => setOpenModal(true)}>Toggle modal</Button>
       <Modal show={openModal} onClose={() => setOpenModal(false)}>
-        <Modal.Header>Terms of Service</Modal.Header>
+        <Modal.Header>{title}</Modal.Header>
+
         <Modal.Body>
           <div className="space-y-6">{children}</div>
         </Modal.Body>
+
         <Modal.Footer>
+
           {showSubmitButton && <Button onClick={() => setOpenModal(false)}>{submitTitle}</Button>}
+
           { showCancelButton && <Button color="gray" onClick={() => setOpenModal(false)}>
-            Cancel
+            {cancelTitle}
           </Button>}
+          
         </Modal.Footer>
+
       </Modal>
     </>
   );
--- src/pages/patients/patientId.tsx
+++ src/pages/patients/patientId.tsx
@@ -1,9 +1,24 @@
 import PatientInfoHeader from "../../components/patientInfoHeader";
+import BasicModal from "../../components/ui/modals/basicModal";
 
 function PatientId() {
+
   return (
     <div className="Patients">
-     <PatientInfoHeader/>
+      <PatientInfoHeader />
+      <BasicModal
+        title="New Modal"
+        setOpenModal={() => {
+          true;
+        }}
+        cancelTitle='Reject'
+        openModal={true}
+        showCancelButton={true}
+        submitTitle="shdcdhcbdh"
+        showSubmitButton={true}
+      >
+        chisom
+      </BasicModal>
     </div>
   );
 }
