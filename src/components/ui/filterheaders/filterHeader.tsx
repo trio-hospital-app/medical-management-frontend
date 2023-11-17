c684620b0de2cf63620c8e24b417cf1aef2e3697
chisom chima <chimachisom360@gmail.com>
2023-11-17 08:43:43 +0100

    implemented filter header

--- src/components/ui/filterheaders/filterHeader.tsx
+++ src/components/ui/filterheaders/filterHeader.tsx
@@ -4,9 +4,11 @@ interface FilterHeaderProps {
   children: React.ReactNode;
   title: string;
   buttonTitle: string;
+  resetFilter: () => void;
+  search: () => void;
 }
 
-function FilterHeader({ children, title, buttonTitle }: FilterHeaderProps) {
+function FilterHeader({ children, title, buttonTitle, resetFilter, search }: FilterHeaderProps) {
   return (
     <div className="bg-white px-5 py-3 rounded-[1rem] shadow-lg">
       <div className="flex items-center justify-between pb-2 border-b">
@@ -20,11 +22,14 @@ function FilterHeader({ children, title, buttonTitle }: FilterHeaderProps) {
         <div className="w-[25%] flex items-center justify-between">
           <span
             className="border border-red-500 rounded-[1rem] w-[49%] h-10 flex items-center justify-center font-bold text-red-500 cursor-pointer hover:bg-red-100"
-            onClick={() => {}}
+            onClick={resetFilter}
           >
             Reset
           </span>
-          <span className="border border-blue-500 rounded-[1rem] w-[49%] h-10 flex items-center justify-center font-bold text-blue-500 cursor-pointer hover:bg-blue-100">
+          <span
+            className="border border-blue-500 rounded-[1rem] w-[49%] h-10 flex items-center justify-center font-bold text-blue-500 cursor-pointer hover:bg-blue-100"
+            onClick={search}
+          >
             Search
           </span>
         </div>
--- src/pages/patients/index.tsx
+++ src/pages/patients/index.tsx
@@ -3,46 +3,73 @@ import FilterHeader from "../../components/ui/filterheaders/filterHeader";
 function Patients() {
   return (
     <div className="Patients">
-      <FilterHeader title="Patient Records" buttonTitle="Create New Patient">
-        <form className="flex flex-wrap gap-2 justify-between">
-          <input
-            id="email1"
-            type="email"
-            placeholder="name@flowbite.com"
-            required
-            className="w-[24%]"
-          />
-          <input
-            id="email1"
-            type="email"
-            placeholder="name@flowbite.com"
-            required
-            className="w-[24%]"
-          />
-          <input
-            id="email1"
-            type="email"
-            placeholder="name@flowbite.com"
-            required
-            className="w-[24%]"
-          />
-          <input
-            id="email1"
-            type="email"
-            placeholder="name@flowbite.com"
-            required
-            className="w-[24%]"
-          />
-          <input
-            id="email1"
-            type="email"
-            placeholder="name@flowbite.com"
-            required
-            className="w-[24%]"
-          />
+      <FilterHeader title="Patient Records" buttonTitle="Create New Patient" resetFilter={()=>'hello'} search={()=>'i am a function'}>
+        <form className="flex flex-wrap gap-2">
+          <div className="w-[24%]">
+            <div className="mb-2 block">
+              <label htmlFor="patientid">Patient ID</label>
+            </div>
+            <input
+              id="patientid"
+              className="w-full"
+              required
+            />
+          </div>
+          <div className="w-[24%]">
+            <div className="mb-2 block">
+              <label htmlFor="firstName">First Name</label>
+            </div>
+            <input
+              id="firstName"
+              className="w-full"
+              required
+            />
+          </div>
+          <div className="w-[24%]">
+            <div className="mb-2 block">
+              <label htmlFor="lastName">Last Name</label>
+            </div>
+            <input
+              id="lastName"
+              required
+              className="w-full"
+            />
+          </div>
+          <div className="w-[24%]">
+            <div className="mb-2 block">
+              <label htmlFor="username">Patient ID</label>
+            </div>
+            <input
+              id="username"
+              className="w-full"
+              placeholder="name@company.com"
+              required
+            />
+          </div>
+          <div className="w-[24%]">
+            <div className="mb-2 block">
+              <label htmlFor="username">Patient ID</label>
+            </div>
+            <input
+              id="username"
+              className="w-full"
+              placeholder="name@company.com"
+              required
+            />
+          </div>
+          <div className="w-[24%]">
+            <div className="mb-2 block">
+              <label htmlFor="username">Patient ID</label>
+            </div>
+            <input
+              id="username"
+              className="w-full"
+              placeholder="name@company.com"
+              required
+            />
+          </div>
         </form>
       </FilterHeader>
-      
     </div>
   );
 }
