64beb2e209f87ad5c19c465f7dbb11155a7c100d
chisom chima <chimachisom360@gmail.com>
2023-11-17 08:27:37 +0100

    implemented filter header

--- src/components/ui/filterheaders/filterHeader.tsx
+++ src/components/ui/filterheaders/filterHeader.tsx
@@ -8,15 +8,15 @@ interface FilterHeaderProps {
 
 function FilterHeader({ children, title, buttonTitle }: FilterHeaderProps) {
   return (
-    <div className="bg-white px-5 py-2 rounded-[1rem] shadow-lg">
-      <div className="flex items-center justify-between pb-1 border-b">
+    <div className="bg-white px-5 py-3 rounded-[1rem] shadow-lg">
+      <div className="flex items-center justify-between pb-2 border-b">
         <span className="text-xl font-bold">{title}</span>
         <Button color="blue">{buttonTitle}</Button>
       </div>
 
-      <div className="p-2">{children}</div>
+      <div className="p-3">{children}</div>
 
-      <div className="w-full pt-1 border-t">
+      <div className="w-full pt-2 border-t">
         <div className="w-[25%] flex items-center justify-between">
           <span
             className="border border-red-500 rounded-[1rem] w-[49%] h-10 flex items-center justify-center font-bold text-red-500 cursor-pointer hover:bg-red-100"
