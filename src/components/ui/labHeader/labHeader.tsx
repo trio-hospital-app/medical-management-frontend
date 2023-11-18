2c8f7d0fca8c84cba640a07b93f091d1a2a597d6
christopherabraham8@gmail.com <christopherabraham8@gmail.com>
2023-11-18 16:46:53 +0100

    create custom lab header

---
+++ src/components/ui/labHeader/labHeader.tsx
@@ -0,0 +1,150 @@
+import { Avatar } from "flowbite-react";
+
+interface TakeSpecimenProps {
+  patientName: string;
+  patientID: string;
+  testName: string;
+  labID: string;
+  gender: string;
+  phoneNumber: string;
+  religion: string;
+  nationality: string;
+  maritalStatus: string;
+  age: string;
+  orderedBy: string;
+  orderedDate: string;
+  testNameBackgroundColor?: string;
+}
+
+function LabHeader({
+  patientName,
+  patientID,
+  testName,
+  labID,
+  gender,
+  phoneNumber,
+  religion,
+  nationality,
+  maritalStatus,
+  age,
+  orderedBy,
+  orderedDate,
+  testNameBackgroundColor,
+}: TakeSpecimenProps) {
+  return (
+    <>
+      <div className="Patients">
+        <div className="bg-white px-10 py-4 border-b mb-5">
+          <div className="flex md:flex-row flex-col md:items-center justify-between py-1 border-b">
+            <div className="flex items-start justify-center flex-col">
+              <span className="md:text-xl font-bold text-bold">
+                {patientName}
+              </span>
+              <div>
+                <label
+                  htmlFor="labID"
+                  className="text-sm font-semibold text-gray-500"
+                >
+                  Paitent ID:
+                </label>
+                <span id="labID" className="text-ha-primary1 pl-1">
+                  {patientID}
+                </span>
+              </div>
+            </div>
+            <div className="flex items-start justify-center flex-col">
+              <span
+                className={`md:text-xl font-bold text-bold px-3 py-1 text-white rounded-[1rem] text-justify ${
+                  testNameBackgroundColor || "bg-pink-400"
+                }`}
+              >
+                {" "}
+                {testName}
+              </span>
+              <div className="px-2">
+                <label
+                  htmlFor="labID"
+                  className="text-sm font-semibold text-gray-500"
+                >
+                  Lab ID:
+                </label>
+                <span id="labID" className="text-ha-primary1 pl-1">
+                  {labID}
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col md:flex-row  md:items-start justify-between mt-5">
+            <div>
+              <Avatar
+                alt="User settings"
+                img="https://cdn-icons-png.flaticon.com/512/666/666201.png"
+                rounded
+                bordered
+                size="xl"
+              />
+            </div>
+            <div className="flex items-start justify-center gap-2 flex-col ">
+              <div className="flex items-start justify-center flex-col">
+                <span className="text-sm font-semibold text-gray-500">
+                  Gender:
+                </span>
+                <span className="font-semibold">{gender}</span>
+              </div>
+              <div className="flex items-start justify-center flex-col">
+                <span className="text-sm font-semibold text-gray-500">
+                  Phone Number:
+                </span>
+                <span className="font-semibold">{phoneNumber}</span>
+              </div>
+            </div>
+
+            <div className="flex items-start justify-center gap-2 flex-col ">
+              <div className="flex items-start justify-center flex-col">
+                <span className="text-sm font-semibold text-gray-500">
+                  Religion:
+                </span>
+                <span className="font-semibold">{religion}</span>
+              </div>
+              <div className="flex items-start justify-center flex-col">
+                <span className="text-sm font-semibold text-gray-500">
+                  Nationality:
+                </span>
+                <span className="font-semibold">{nationality}</span>
+              </div>
+            </div>
+
+            <div className="flex items-start justify-center gap-2 flex-col pr-10">
+              <div className="flex items-start justify-center flex-col">
+                <span className="text-sm font-semibold text-gray-500">
+                  Marital Status:
+                </span>
+                <span className="font-semibold">{maritalStatus}</span>
+              </div>
+              <div className="flex items-start justify-center flex-col">
+                <span className="text-sm font-semibold text-gray-500">Age</span>
+                <span className="font-semibold">{age}</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="px-10 py-1 shadow flex items-start justify-start gap-5 flex-col md:flex-row">
+          <div className="flex items-start justify-center flex-col">
+            <span className="text-sm font-semibold text-gray-500">
+              Orderd By:
+            </span>
+            <span className="font-semibold">{orderedBy}</span>
+          </div>
+          <div className="flex items-start justify-center flex-col">
+            <span className="text-sm font-semibold text-gray-500">
+              Orderd Date:
+            </span>
+            <span className="font-semibold">{orderedDate}</span>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default LabHeader;
