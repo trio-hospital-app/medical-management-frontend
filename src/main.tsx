ff8c8e76580856caee97f002f7c08849114e23f6
chisom chima <chimachisom360@gmail.com>
2023-11-23 09:28:38 +0100

    react query added

--- src/main.tsx
+++ src/main.tsx
@@ -4,26 +4,26 @@ import App from "./App.tsx";
 import "./global.css";
 import { BrowserRouter } from "react-router-dom";
 import DesignerContextProvider from "./pages/settings/forms/components/context/DesignerContext.tsx";
-import { ReactQueryDevtools } from 'react-query/devtools'
+import { ReactQueryDevtools } from "react-query/devtools";
 import {
   // useQuery,
   // useMutation,
   // useQueryClient,
   QueryClient,
   QueryClientProvider,
-} from 'react-query'
+} from "react-query";
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-     <QueryClientProvider client={queryClient}>
-    <BrowserRouter>
-      <DesignerContextProvider>
-        <App />
-      </DesignerContextProvider>
-    </BrowserRouter>
-    <ReactQueryDevtools initialIsOpen={false}  position="bottom-right" />
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <DesignerContextProvider>
+          <App />
+        </DesignerContextProvider>
+      </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
     </QueryClientProvider>
   </React.StrictMode>
 );
