53f644d6cc07416c375a3a9bfb3b3dd0e643eff0
chisom chima <chimachisom360@gmail.com>
2024-02-12 20:27:12 +0100

    sidebar links

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -1,5 +1,5 @@
 import { Dropdown, Sidebar } from "flowbite-react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { MdBloodtype } from "react-icons/md";
 // import { FaUserNurse } from "react-icons/fa";
 import { GiMedicines } from "react-icons/gi";
@@ -8,39 +8,47 @@ import { FaUserDoctor } from "react-icons/fa6";
 import { IoIosPeople } from "react-icons/io";
 import { MdDashboard } from "react-icons/md";
 import { IoMdSettings } from "react-icons/io";
-import { GiReceiveMoney } from "react-icons/gi";
+import { GiReceiveMoney } from "react-icons/gi";  
 
 function SideBar() {
+
+
+const location = useLocation();
+
+const isLinkActive = (path) => {
+  return location.pathname.includes(path);
+};
+
   return (
     <div className="h-full custom-bg-black shadow-md z-[20]">
       <Sidebar aria-label="Sidebar with multi-level dropdown example">
         <Sidebar.Items>
           <Sidebar.ItemGroup>
-            <Sidebar.Item as={Link} to="/dashboard" icon={MdDashboard}>
+            <Sidebar.Item as={Link} className={isLinkActive('dashboard') && 'bg-indigo-500'} to="/dashboard" icon={MdDashboard}>
               <span className="hidden lg:flex">Dashboard</span>
             </Sidebar.Item>
-            <Sidebar.Item as={Link} to="/patients" icon={IoIosPeople}>
+            <Sidebar.Item as={Link} className={isLinkActive('patients') && 'bg-indigo-500'} to="/patients" icon={IoIosPeople}>
               <span className="hidden lg:flex">Patients</span>
             </Sidebar.Item>
-            <Sidebar.Item as={Link} to="/visits" icon={FaUserDoctor}>
+            <Sidebar.Item as={Link} className={isLinkActive('visits') && 'bg-indigo-500'} to="/visits" icon={FaUserDoctor}>
               <span className="hidden lg:flex">Consultation</span>
             </Sidebar.Item>
-            <Sidebar.Item as={Link} to="/laboratory" icon={MdBloodtype}>
+            <Sidebar.Item as={Link} className={isLinkActive('laboratory') && 'bg-indigo-500'} to="/laboratory" icon={MdBloodtype}>
               <span className="hidden lg:flex">Laboratory</span>
             </Sidebar.Item>
-            <Sidebar.Item as={Link} to="/radiology" icon={GiSkeleton}>
+            <Sidebar.Item as={Link} className={isLinkActive('radiology') && 'bg-indigo-500'} to="/radiology" icon={GiSkeleton}>
               <span className="hidden lg:flex">Radiology</span>
             </Sidebar.Item>
-            <Sidebar.Item as={Link} to="/pharmacy" icon={GiMedicines}>
+            <Sidebar.Item as={Link} className={isLinkActive('pharmacy') && 'bg-indigo-500'} to="/pharmacy" icon={GiMedicines}>
               <span className="hidden lg:flex">Pharmacy</span>
             </Sidebar.Item>
             {/* <Sidebar.Item as={Link} to="/nursing" icon={FaUserNurse}>
               <span className="hidden lg:flex">Nursing</span>
             </Sidebar.Item> */}
-            <Sidebar.Item as={Link} to="/finance" icon={GiReceiveMoney}>
+            <Sidebar.Item as={Link} className={isLinkActive('finance') && 'bg-indigo-500'} to="/finance" icon={GiReceiveMoney}>
               <span className="hidden lg:flex">Finance</span>
             </Sidebar.Item>
-            <Sidebar.Item icon={IoMdSettings}>
+            <Sidebar.Item icon={IoMdSettings} className={isLinkActive('settings') && 'bg-indigo-500'} >
               <span className="hidden lg:flex">
                 {" "}
                 <Dropdown arrowIcon={true} inline label="Settings">
