c4e85e5891aa5d8f3bc4511ca7840f9094fa01e4
christopherabraham8@gmail.com <christopherabraham8@gmail.com>
2024-04-03 10:39:01 +0100

    fix doubke re-rebding

--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,7 @@ import { QueryClient, QueryClientProvider } from "react-query";
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
+  <>
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
         <App />
@@ -19,5 +19,5 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
       </BrowserRouter>
       <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
     </QueryClientProvider>
-  </React.StrictMode>,
+  </>
 );
